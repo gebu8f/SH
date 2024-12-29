90ce5ff042e3b98a10a098813e3aa07d3c3454f0
gebu8f <125874137+gebu8f@users.noreply.github.com>
2024-12-29 10:34:23 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -2,4 +2,4 @@ sudo mkdir /root/cert
 sudo apt install openssl -y
 sudo openssl req -x509 -nodes -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -keyout /root/cert/default_server.key -out /root/cert/default_server.crt -days 5475 -subj "/C=US/ST=State/L=City/O=Organization/OU=Organizational Unit/CN=Common Name"
 rm /etc/nginx/sites-available/default
-wget 
+wget -O default https://raw.githubusercontent.com/gebu8f/SH/refs/heads/main/default_system /etc/nginx/sites-available
