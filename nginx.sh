185220e57c2881db5db97f1e2d9f84c05bed8ce7
gebu8f <125874137+gebu8f@users.noreply.github.com>
2024-12-29 10:54:38 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,4 @@
+sudo mkdir /home/web/
 sudo mkdir /home/web/cert
 sudo apt install openssl -y
 sudo openssl req -x509 -nodes -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -keyout /home/web/cert/default_server.key -out /home/web/cert/default_server.crt -days 5475 -subj "/C=US/ST=State/L=City/O=Organization/OU=Organizational Unit/CN=Common Name"
